178f63c6222f19afe04b2b15dcbd2e95edf055b4
VincenzoLomba <vincenzolombardi99@gmail.com>
2023-03-19 14:46:37 +0100

    Update sshConnectionToServer.sh

--- developing/sshConnectionToServer.sh
+++ developing/sshConnectionToServer.sh
@@ -5,7 +5,7 @@ if [ -z "${CBN_PEM}" ]; then
 	echo " An environment variable named CBN_PEM has to be defined!"
 	echo " This environment variable must contain the full path of the PEM file (to use in order to establish the ssh connection)"
 else
-	SERVER_DOMAIN=campusbynight.schoolcare.it
+	SERVER_DOMAIN=campusbynight.it
 	dashes=`eval printf -- '-%.0s' {1..${#SERVER_DOMAIN}}` #(https://stackoverflow.com/a/23087599/4244130)
 	echo ""
 	echo "+---------------------------------------${dashes}+"
