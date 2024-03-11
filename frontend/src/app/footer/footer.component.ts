97d34ebc90ce1499bcd3a68548d4be2c2bb334da
GiuseppeBondii <131357828+GiuseppeBondii@users.noreply.github.com>
2024-03-11 12:38:35 +0100

    update

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
- 
+  public showImage: boolean = false;
+  public tenclick: number = 15;
+
+  SHOW(){
+    if(this.tenclick==0){
+      this.showImage=true;
+    }
+  }
+
+  toggleImage() {
+    this.tenclick = this.tenclick -1;
+    this.SHOW();
+  }
 }
