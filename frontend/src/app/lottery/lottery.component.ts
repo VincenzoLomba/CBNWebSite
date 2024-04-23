24542daa343b83016ad4ea705b1207cf89e8918f
VincenzoLomba <vincenzolombardi99@gmail.com>
2024-04-23 16:15:07 +0200

    Little Lottery Component Update

--- frontend/src/app/lottery/lottery.component.ts
+++ frontend/src/app/lottery/lottery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService, Prize } from '../services/data.service';
 
 @Component({
@@ -9,20 +9,5 @@ import { DataService, Prize } from '../services/data.service';
 export class LotteryComponent {
 
   constructor(public dataService: DataService) {}
-
   getPrizes(): Array<Prize> { return this.dataService.getPrizes(); }
-
-  public showImage: boolean = false;
-  public tenclick: number = 15;
-
-  SHOW(){
-    if(this.tenclick<=0){
-      this.showImage=true;
-    }
-  }
-
-  toggleImage() {
-    this.tenclick = this.tenclick -1;
-    this.SHOW();
-  }
 }
