fd4c06843095107afd93952545adf638c9d4e7d7
VincenzoLomba <vincenzolombardi99@gmail.com>
2024-05-19 17:38:39 +0200

    Little Lottery Update

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -75,80 +75,80 @@ export class DataService {
 
   getPrizes(): Array<Prize> {
     let response = `
-    Scooter Kymco Agility 125cc
-    MacBook Air 13’’ con chip Apple M1, 8 GB di RAM e 256 GB di SSD
-    Soggiorno di una settimana @Castellammare del Golfo (TP)
-    Soggiorno di due notti (15-17/06) con colazione @Agriturismo tenuta il Cicalino, Massa Marittima (GR)
-    Credito dal valore di 300€ per noleggio autoveicoli @Corrente
-    Buono da 300€ spendibile per abito da cerimonia @Tosca spose
-    Tre anelli in argento fatti a mano, collezione ad-honorem offerti da @Beg oreficieria
-    Abbonamento di 6 mesi in palestra offerto da @Sinergy
-    Casco integrale HJC offerto da @Guelfa 76
-    Soggiorno di due notti presso "Locanda Alfieri", Termoli
-    Tre anelli in argento fatti a mano, collezione di Bologna offerti da @Beg oreficieria
-    Buono di 140 euro spendibile presso Stabilimenti Balneari Le Spiagge (Rimini)
-    Cena per due presso ristorante Gradale del Castello Monterone (Perugia)
-    Carnet di 11 ingressi gratuiti presso Hyperspace (Bologna)
-    Tre ingressi omaggio presso Acquario di Genova
-    Due ingressi spa per due persone presso Le Rose Suite hotel (Rimini)
-    Cena per due presso agriturismo "Orto di Mari" (Imola)
-    Buono per ombrellone e lettini per quattro giorni presso Bagno Cesare 66 (Riccione)
-    Due box di vini offerti da cantina Paltrinieri
-    Voucher da 70€ di degustazione per due presso "Azienda agricola Palazzo di Varignana" 
-    Kit Setern (tonico e siero viso) offerto da Terme San Petronio
-    Kit Vitabir (latte detergente e siero viso) offerto da Terme San Petronio
-    Escursione equestre per due persone presso "Centro Ippico Cavallo Maremma ASD" (Massa Marittima)
-    Aperitivo-degustazione per due presso Azienda Frontali (Rimini)
-    Set collant Trasparenze da donna offerto da "Nuova Virgiliana"
-    Box di 6 vini offerto da VignaEnaoli
-    Cesta di prodotti agricoli offerti da "Gruppo Grifo Agroalimentare" 
-    Cesta di prodotti agricoli offerti da "Gruppo Grifo Agroalimentare" 
-    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 
-    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 
-    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 
-    Kit di saponi e make up offerto da Farmacia Dal Contavalli
-    Kit di saponi e make up offerto da Farmacia Dal Contavalli
-    Kit di saponi e make up offerto da Farmacia Dal Contavalli
-    Credito dal valore di 50€ spendibile presso Spiller (Bologna)
-    Credito dal valore di 50€ presso libreria Tinarelli (Bologna)
-    Credito spendibile di 50€ per noleggio autoveicoli @Corrente
-    Credito spendibile di 50€ di workshop presso "Mastri Cartai e Dintorni" (Bologna)
-    Kit di dermocosmesi offerta da Terme San Petronio
-    Tre pasti gratuiti a fronte di almeno un pagante presso Yomi Sushi (Bologna)
-    Due salami da Luca Carni (Rimini)
-    Dieci colazioni omaggio presso Caffè dei Orefici (Bologna)
-    Tre barattoli di miele offerti da  Azienda Agricola Scurti
-    Due ingressi omaggio validi per l'intero complesso del Museo Nazionale Dell'Automobile
-    Visita per due persone presso Perugia sotterranea e presso Museo del Capitolo e della Cattedrale offerto da Isola San Lorenzo
-    Crediti di 30€ spendibili presso pizzeria Largo Respighi (Bologna)
-    Due penne biro offerte da Cartoleria Biagini
-    Orecchini fatti a mano da Xii BO Original Design Shop
-    Messa in piega presso "Donna Gio' studio" in via Mazzini, Bologna
-    Brunch del fine settimana offerto da Open Hotel, Rimini
-    Sacchettini di streghette da "Il Banco Del Pane" (Bologna)
-    Sacchettini di streghette da "Il Banco Del Pane" (Bologna)
-    Due biglietti omaggio per partita della squadra Zebre - rugby Parma il 17/05/24
-    Una bottiglia di vino offerta da cantina Michele Satta
-    Cesta di prodotti offerto da Molino Benini (Ravenna)
-    Aperitivo per due offerto da pasticceria Santo Stefano (Bologna)
-    Aperitivo per due (due calici e due piadine) offerto da BottegAmelia (Bologna)
-    10% di sconto dall'Osteria dell'Orsa (Bologna)
-    Ingresso omaggio per giornata co-working, Open Hotel, Rimini
-    Buono per mezzo kilo di gelato offerto da Cremeria Mascarella (Bologna)
-    Buono spesa di 10€ spendibile presso Sumo (Bologna)
-    50% di sconto per escape room presso "Secret Zone"
-    15% fino al 15 giugno presso Eticlò (Bologna)
-    Credito del valore di 10€ presso Alman cicli (Bologna)
-    20% di sconto da La bottega Mi Va (entro 30/06/2024)
-    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)
-    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)
-    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)
-    Tre catene da bici
-    Due spray coloranti per capelli e uno shampoo da parrucchieria
-    Buono spesa di 5€ spendibile da Sumo (Bologna)
-    Abito offerto da Oxigeno
+    Scooter Kymco Agility 125cc	03776
+    MacBook Air 13’’ con chip Apple M1, 8 GB di RAM e 256 GB di SSD	00707
+    Soggiorno di una settimana @Castellammare del Golfo (TP)	06976
+    Soggiorno di due notti (15-17/06) con colazione @Agriturismo tenuta il Cicalino, Massa Marittima (GR)	01743
+    Credito dal valore di 300€ per noleggio autoveicoli @Corrente	07235
+    Buono da 300€ spendibile per abito da cerimonia @Tosca spose	05449
+    Tre anelli in argento fatti a mano, collezione ad-honorem offerti da @Beg oreficieria	02919
+    Abbonamento di 6 mesi in palestra offerto da @Sinergy	05616
+    Casco integrale HJC offerto da @Guelfa 76	02650
+    Soggiorno di due notti presso "Locanda Alfieri", Termoli	02006
+    Tre anelli in argento fatti a mano, collezione di Bologna offerti da @Beg oreficieria	07281
+    Buono di 140 euro spendibile presso Stabilimenti Balneari Le Spiagge (Rimini)	06995
+    Cena per due presso ristorante Gradale del Castello Monterone (Perugia)	02359
+    Carnet di 11 ingressi gratuiti presso Hyperspace (Bologna)	04185
+    Tre ingressi omaggio presso Acquario di Genova	07829
+    Due ingressi spa per due persone presso Le Rose Suite hotel (Rimini)	00609
+    Cena per due presso agriturismo "Orto di Mari" (Imola)	04599
+    Buono per ombrellone e lettini per quattro giorni presso Bagno Cesare 66 (Riccione)	07415
+    Due box di vini offerti da cantina Paltrinieri	02400
+    Voucher da 70€ di degustazione per due presso "Azienda agricola Palazzo di Varignana" 	02032
+    Kit Setern (tonico e siero viso) offerto da Terme San Petronio	07084
+    Kit Vitabir (latte detergente e siero viso) offerto da Terme San Petronio	04617
+    Escursione equestre per due persone presso "Centro Ippico Cavallo Maremma ASD" (Massa Marittima)	02314
+    Aperitivo-degustazione per due presso Azienda Frontali (Rimini)	00316
+    Set collant Trasparenze da donna offerto da "Nuova Virgiliana"	07215
+    Box di 6 vini offerto da VignaEnaoli	00120
+    Cesta di prodotti agricoli offerti da "Gruppo Grifo Agroalimentare" 	01078
+    Cesta di prodotti agricoli offerti da "Gruppo Grifo Agroalimentare" 	06965
+    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 	02760
+    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 	07275
+    Portafoglio offerto da Pelletteria Campora con set di cinture abbinato 	02678
+    Kit di saponi e make up offerto da Farmacia Dal Contavalli	06546
+    Kit di saponi e make up offerto da Farmacia Dal Contavalli	05251
+    Kit di saponi e make up offerto da Farmacia Dal Contavalli	02914
+    Credito dal valore di 50€ spendibile presso Spiller (Bologna)	03823
+    Credito dal valore di 50€ presso libreria Tinarelli (Bologna)	04535
+    Credito spendibile di 50€ per noleggio autoveicoli @Corrente	05728
+    Credito spendibile di 50€ di workshop presso "Mastri Cartai e Dintorni" (Bologna)	03842
+    Kit di dermocosmesi offerta da Terme San Petronio	07856
+    Tre pasti gratuiti a fronte di almeno un pagante presso Yomi Sushi (Bologna)	02923
+    Due salami da Luca Carni (Rimini)	05908
+    Dieci colazioni omaggio presso Caffè dei Orefici (Bologna)	00435
+    Tre barattoli di miele offerti da  Azienda Agricola Scurti	01754
+    Due ingressi omaggio validi per l'intero complesso del Museo Nazionale Dell'Automobile	03731
+    Visita per due persone presso Perugia sotterranea e presso Museo del Capitolo e della Cattedrale offerto da Isola San Lorenzo	01849
+    Crediti di 30€ spendibili presso pizzeria Largo Respighi (Bologna)	00321
+    Due penne biro offerte da Cartoleria Biagini	01889
+    Orecchini fatti a mano da Xii BO Original Design Shop	00479
+    Messa in piega presso "Donna Gio' studio" in via Mazzini, Bologna	07213
+    Brunch del fine settimana offerto da Open Hotel, Rimini	01768
+    Sacchettini di streghette da "Il Banco Del Pane" (Bologna)	01327
+    Sacchettini di streghette da "Il Banco Del Pane" (Bologna)	07681
+    Due biglietti omaggio per partita della squadra Zebre - rugby Parma il 17/05/24	02008
+    Una bottiglia di vino offerta da cantina Michele Satta	03094
+    Cesta di prodotti offerto da Molino Benini (Ravenna)	06538
+    Aperitivo per due offerto da pasticceria Santo Stefano (Bologna)	06424
+    Aperitivo per due (due calici e due piadine) offerto da BottegAmelia (Bologna)	00721
+    10% di sconto dall'Osteria dell'Orsa (Bologna)	02038
+    Ingresso omaggio per giornata co-working, Open Hotel, Rimini	01225
+    Buono per mezzo kilo di gelato offerto da Cremeria Mascarella (Bologna)	07851
+    Buono spesa di 10€ spendibile presso Sumo (Bologna)	01700
+    50% di sconto per escape room presso "Secret Zone"	02796
+    15% fino al 15 giugno presso Eticlò (Bologna)	07499
+    Credito del valore di 10€ presso Alman cicli (Bologna)	06635
+    20% di sconto da La bottega Mi Va (entro 30/06/2024)	03413
+    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)	02381
+    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)	00254
+    Buono per degustazione per due di focacce da "La Slerfa Focacceria Genovese" (Bologna)	00311
+    Tre catene da bici	07452
+    Due spray coloranti per capelli e uno shampoo da parrucchieria	02481
+    Buono spesa di 5€ spendibile da Sumo (Bologna)	07383
+    Abito offerto da Oxigeno	06593
     `;
-    return response.split('\n').filter(el => el.trim() != '').map(p => new Prize(null, p.trim()));
+    return response.split('\n').filter(el => el.trim() != '').map(p => new Prize(p.split('	')[0].trim(), p.split('	')[1].trim()));
   }
 }
 
